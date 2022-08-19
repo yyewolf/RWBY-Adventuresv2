2f14ea5e722ff3b28ab6b7d1501a5b78417422c0
Tristan SMAGGHE <tristan.smagghe@telecomnancy.eu>
2022-08-19 22:12:06 +0200

    [+] Arena front start

---
+++ arenas_front/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-app-bar elevation="4">
+      <v-row justify="center">
+        <h1 class="text-primary">Hello, {{username}}</h1>
+      </v-row>
+    </v-app-bar>
+    <v-main>
+      <Arena :arena="arena" />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Arena from './components/Arena.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    Arena,
+  },
+
+  data: () => ({
+    username : 'Yewolf',
+    arena : {
+      title: "You have to defeat a Grimm",
+      people: 1,
+      image: "https://www.pngfind.com/pngs/m/78-781864_nevermore-rwby-png-download-rwby-volume-4-grimm.png"
+    }
+  }),
+}
+</script>
---
+++ arenas_front/src/components/Arena.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-row class="mb-3" justify="center">
+      <h2>{{arena.title}}</h2>
+    </v-row>
+    <v-row class="mb-5" justify="center">
+      <h4>There are {{arena.people}} people with you.</h4>
+    </v-row>
+    <v-row justify="center">
+      <v-card width="20vw" @click="hit++">
+        <v-card-text>
+          <v-img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8bc5f32c-8fbf-4166-8729-b7d4d62cb35d/ddbvlct-86de9502-195a-4185-a02d-41432f9d19e9.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzhiYzVmMzJjLThmYmYtNDE2Ni04NzI5LWI3ZDRkNjJjYjM1ZFwvZGRidmxjdC04NmRlOTUwMi0xOTVhLTQxODUtYTAyZC00MTQzMmY5ZDE5ZTkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.593heLTyYUe6rYeftcflrOjPyGVBnopJCEl_gfllkAo"/>
+        </v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'ArenaPage',
+  props: ["arena"],
+  data: () => ({
+    hit:0,
+  }),
+}
+</script>
